c9656af7c27343c5d834503409e33d987203e9e3
olegesan <37033572+olegesan@users.noreply.github.com>
2019-12-23 15:29:58 -0500

    something works perhaps

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const APIlink = 'https://gist.githubusercontent.com/b1nary/ea8fff806095bcedacce/raw/6e6de20d7514b93dd69b149289264997b49459dd/enterpreneur-quotes.json'
+const APIlink = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 const randomNum=(length)=>(Math.floor(Math.random()*length))
-
+const colorSchemes=[['706698','F3F5F4'],['201F28','E4E6E1'],['DF7B86','FAFAF9'], ['1FABC2','F6F8F7']]
 class App extends React.Component{
   constructor(props){
     super(props);
@@ -17,35 +17,50 @@ class App extends React.Component{
     let quotes = await fetch(APIlink)
     .then(data=>data.json())    
     .then(data=>{ this.setState(
-     {quotes:data,numOfQt:data.length}
+     {quotes:data.quotes,numOfQt:data.quotes.length}
     )
-    return data
+    return data.quotes
     })
     let rndQtID=randomNum(quotes.length)
-    let quote=quotes[rndQtID].text
-    let author=quotes[rndQtID].from
+    let quote=`"${quotes[rndQtID].quote}"`
+    let author=quotes[rndQtID].author
     this.setState({quote,author})
-    console.log(this.state)
   }
   getNewQuote(){
     const newQtID = randomNum(this.state.numOfQt)
-    let quote = this.state.quotes[newQtID].text
-    let author = this.state.quotes[newQtID].from
+    let quote = `"${this.state.quotes[newQtID].quote}"`
+    let author = this.state.quotes[newQtID].author
     this.setState({quote,author})
   }
   componentWillMount(){
     this.getQuotes();
   }
   render(){
+    const style = ()=>{
+    let randomPallet = colorSchemes[Math.floor(Math.random()*colorSchemes.length)]
+    return {backgroundColor:`#${randomPallet[0]}`,
+            color:`#${randomPallet[1]}`
+            }
+    }
+    const {color,backgroundColor}=style()
+    const inBoxStyle = {color,backgroundColor}
+    
     return(
-      <div id='quote-box'className='App'>
-        <h1 id='text'>
-          {this.state.quote}
-        </h1>
-        <h3 id='author'>by {this.state.author}</h3>
-        <button id='new-quote'onClick={this.getNewQuote}>New Quote</button>
-        <br/>
-        <a href="https://twitter.com/intent/tweet" id='tweet-quote'>[tweet]</a>
+      <div className='container' style={{width:'100vw', height:'100vh',color, backgroundColor}}>
+        <div id='quote-box'className='App' style={{backgroundColor:color, color:backgroundColor}}>
+
+           <h1 id='text'>
+            {`${this.state.quote}`}
+          </h1>
+          <h3 id='author'>by {this.state.author}</h3>
+      
+         
+          <div className='actions'>
+            <button id='new-quote' style={inBoxStyle} onClick={this.getNewQuote}>New Quote</button>
+            <br/>
+            <a style={inBoxStyle} href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${encodeURIComponent(this.state.quote)} ${encodeURIComponent(this.state.author)}`} id='tweet-quote'>tweet</a>
+          </div>
+        </div>
       </div>
       )
     }
